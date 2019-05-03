3531710cef707266f381c3701ac8e7d0d3820a3e
Mark Piper <mark.piper@colorado.edu>
2019-05-03 12:12:07 -0600

    Update pymt example
    With the addition of model metadata files, I can get through the
    setup step. Initialize fails, though; I need to reconcile the config
    file with the CGNS input data file.

--- examples/fastmech_pymt_ex.py
+++ examples/fastmech_pymt_ex.py
@@ -6,3 +6,22 @@ from pymt.models import FaSTMECH
 # Instantiate the model and get its name.
 m = FaSTMECH()
 print(m.name)
+
+# List input and output variable names.
+print('Input variable names:')
+for name in m.input_var_names:
+    print(' - {}'.format(name))
+print('Output variable names:')
+for name in m.output_var_names:
+    print(' - {}'.format(name))
+
+# Call setup to get default config and data files.
+defaults = m.setup('.')
+print('Setup:')
+for item in defaults:
+    print(' - {}'.format(item))
+
+# Initialize the model with the defaults.
+# m.initialize(*defaults) -> segfault
+# m.initialize('') -> segfault
+# m.initialize('Test1.cgn') -> grid type error 'structured_quadrilateral'
